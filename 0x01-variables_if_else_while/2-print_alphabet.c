52b3b93c2ffdbe2e66608cc4535f1394735b8842
Twin-k1e <enatufeavour47@gmail.com>
2022-10-16 05:30:11 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always (0);
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+Return: Always(0);
+}
