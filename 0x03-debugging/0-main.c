4b62e808fe68308210641e5b91ee5a9f0a114415
Twin-k1e <enatufeavour47@gmail.com>
2022-10-19 14:35:05 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main_test function that prints if integer is positive or negative
+ * main-test function that prints if integer is positive or negative
  * Return: 0
  */
 
