4faba272f17868d595c8be99ae2784af5e0df72e
Twin-k1e <enatufeavour47@gmail.com>
2022-10-20 12:24:49 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,35 +1,42 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * print_to_98 - from n to 98
- * @n: integer
- *
- * Return: this function print from n to 98
- */
-void print_to_98(int n)
-{
-	if (n < 98)
-	{
-		for (; n <= 98; n++)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-		}
-	}
-	else
-	{
-		for (; n >= 98; n--)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-		}
-	}
 
-	printf("\n");
+/**
+ * print_times_table - prints time table of n
+ *
+ * @n: takes number input
+ */
+
+void print_times_table(int n)
+{
+	int prod, mult, num;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (num = 0; num <= n; ++num)
+		{
+			_putchar(48);
+			for (mult = 1; mult <= n; ++mult)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 9)
+					_putchar(' ');
+				if (prod <= 99)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
+				}
+				else if (prod <= 99 && prod >= 10)
+					_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
+			}
+			_putchar('n');
+		}
+	}
 }
